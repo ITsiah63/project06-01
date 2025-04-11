90c9f42bcde66c894909f0d3735912177d7333e1
Isiah Reyes <i0576631@amarillocollege.com>
2025-04-10 21:27:43 -0500

    first commit

---
+++ project06-01.js
@@ -0,0 +1,28 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-01
+
+      Project to validate a form used for setting up a new account
+      Author: Isiah Reyes
+      Date:   04-09-25
+
+      Filename: project06-01.js
+*/
+
+let submitButton = document.getElementById("submitButton");
+let pwd = document.getElementById("pwd");
+let pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", anonymous);
+
+function anonymous() {
+      if (document.getElementById("pwd").validity.rangeUnderflow) {
+            text = ("Your password must be at least 8 characters with at least one letter and number");
+      } else if (document.getElementById("pwd").validity.patternMismatch) {
+            text = ("Your passwords don't match");
+      } else {
+            pwd.setCustomValidity("");
+      }
+}
+
